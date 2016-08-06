a92de4f8e59f13597ed6f7af1549d96fe389e3a1
Brett Bennett <b_j_bennett@yahoo.com>
2016-08-06 15:03:33 -0700

    Update pyRW.py

--- pyRW.py
+++ pyRW.py
@@ -2,11 +2,9 @@ import os, sys, codecs
 from os import write
 os.chdir('//VBOXSVR/My_Passport')
 
-for path, subdirs, files in os.walk(r'//VBOXSVR/My_Passport'):
-    for filename in files:
-        f = os.path.join(path, filename)        
-        print(str(f))
-
-
-with open('//VBOXSVR/My_Passport/cat,txt', "w") as text_file:
-    text_file.writelines(str(f))
+with open('//VBOXSVR/My_Passport/cat.txt', "w") as text_file:
+    for path, subdirs, files in os.walk(r'//VBOXSVR/My_Passport'):
+        for filename in files:
+            f = os.path.join(path, filename)        
+            print(str(f))
+            text_file.writelines(str(f))
